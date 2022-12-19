19f65f8a25fabb4a3ba7e54de4f36818d9f28917
Francis Martel <1328322@cegepoutaouais.qc.ca>
2022-12-18 20:26:13 -0500

    Ajout des formulaires de base nécessaire au fonctionnement du programme

--- Formulaires/FormReservation.Designer.cs
+++ Formulaires/FormReservation.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjetSynthese.Formulaires
 {
-    partial class FormReservation
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajoutClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modifClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.réservationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajoutResevToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modifResevToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.annulerReservToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.afficherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listReservsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spaStatsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientToolStripMenuItem,
+            this.réservationToolStripMenuItem,
+            this.afficherToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clientToolStripMenuItem
+            // 
+            this.clientToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajoutClientToolStripMenuItem,
+            this.modifClientToolStripMenuItem});
+            this.clientToolStripMenuItem.Name = "clientToolStripMenuItem";
+            this.clientToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.clientToolStripMenuItem.Text = "Client";
+            // 
+            // ajoutClientToolStripMenuItem
+            // 
+            this.ajoutClientToolStripMenuItem.Name = "ajoutClientToolStripMenuItem";
+            this.ajoutClientToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ajoutClientToolStripMenuItem.Text = "Créer un client";
+            this.ajoutClientToolStripMenuItem.Click += new System.EventHandler(this.ajoutClientToolStripMenuItem_Click);
+            // 
+            // modifClientToolStripMenuItem
+            // 
+            this.modifClientToolStripMenuItem.Name = "modifClientToolStripMenuItem";
+            this.modifClientToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modifClientToolStripMenuItem.Text = "Modifier un client";
+            this.modifClientToolStripMenuItem.Click += new System.EventHandler(this.modifClientToolStripMenuItem_Click);
+            // 
+            // réservationToolStripMenuItem
+            // 
+            this.réservationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajoutResevToolStripMenuItem,
+            this.modifResevToolStripMenuItem,
+            this.annulerReservToolStripMenuItem});
+            this.réservationToolStripMenuItem.Name = "réservationToolStripMenuItem";
+            this.réservationToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.réservationToolStripMenuItem.Text = "Réservation";
+            // 
+            // ajoutResevToolStripMenuItem
+            // 
+            this.ajoutResevToolStripMenuItem.Name = "ajoutResevToolStripMenuItem";
+            this.ajoutResevToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.ajoutResevToolStripMenuItem.Text = "Créer une réservation";
+            this.ajoutResevToolStripMenuItem.Click += new System.EventHandler(this.ajoutResevToolStripMenuItem_Click);
+            // 
+            // modifResevToolStripMenuItem
+            // 
+            this.modifResevToolStripMenuItem.Name = "modifResevToolStripMenuItem";
+            this.modifResevToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.modifResevToolStripMenuItem.Text = "Modifier une réservation";
+            this.modifResevToolStripMenuItem.Click += new System.EventHandler(this.modifResevToolStripMenuItem_Click);
+            // 
+            // annulerReservToolStripMenuItem
+            // 
+            this.annulerReservToolStripMenuItem.Name = "annulerReservToolStripMenuItem";
+            this.annulerReservToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.annulerReservToolStripMenuItem.Text = "Annuler une réservation";
+            this.annulerReservToolStripMenuItem.Click += new System.EventHandler(this.annulerReservToolStripMenuItem_Click);
+            // 
+            // afficherToolStripMenuItem
+            // 
+            this.afficherToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listReservsToolStripMenuItem,
+            this.spaStatsToolStripMenuItem,
+            this.toolStripSeparator1});
+            this.afficherToolStripMenuItem.Name = "afficherToolStripMenuItem";
+            this.afficherToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.afficherToolStripMenuItem.Text = "Afficher";
+            // 
+            // listReservsToolStripMenuItem
+            // 
+            this.listReservsToolStripMenuItem.Name = "listReservsToolStripMenuItem";
+            this.listReservsToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.listReservsToolStripMenuItem.Text = "Liste des réservations";
+            this.listReservsToolStripMenuItem.Click += new System.EventHandler(this.listReservsToolStripMenuItem_Click);
+            // 
+            // spaStatsToolStripMenuItem
+            // 
+            this.spaStatsToolStripMenuItem.Name = "spaStatsToolStripMenuItem";
+            this.spaStatsToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.spaStatsToolStripMenuItem.Text = "Statistiques du spa";
+            this.spaStatsToolStripMenuItem.Click += new System.EventHandler(this.spaStatsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(182, 6);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormReservation";
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "Gestion de spa";
+            this.Load += new System.EventHandler(this.FormReservation_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajoutClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modifClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem réservationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajoutResevToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modifResevToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem annulerReservToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem afficherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listReservsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spaStatsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
     }
 }
--- Formulaires/FormReservation.cs
+++ Formulaires/FormReservation.cs
@@ -10,11 +10,65 @@ using System.Windows.Forms;
 
 namespace ProjetSynthese.Formulaires
 {
-    public partial class FormReservation : Form
+    public partial class mainForm : Form
     {
-        public FormReservation()
+        public mainForm()
         {
             InitializeComponent();
+        }
+
+        private void FormReservation_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ajoutClientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAjoutClient f = new FormAjoutClient();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void modifClientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormModifClient f = new FormModifClient();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void ajoutResevToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAjoutReserv f = new FormAjoutReserv();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void modifResevToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormModifReserv f = new FormModifReserv();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void annulerReservToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAnnulReserv f = new FormAnnulReserv();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void listReservsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormListReservs f = new FormListReservs();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void spaStatsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormSpaStats f = new FormSpaStats();
+            f.MdiParent = this;
+            f.Show();
         }
     }
 }
